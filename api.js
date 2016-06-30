c6418f8882f62c163211e5342a33638aeac85572
Roman Zhak <zhak55@ya.ru>
2016-06-30 15:06:01 +0300

    Update api.js

--- api.js
+++ api.js
@@ -5,9 +5,12 @@ var api = new API();
  * -- {String}  :
  * --- 1. = 'count'   : Вернуть количество пользователей 
  * --- 2. = 'payment' : Вернуть пользователей, которые совершали покупки 
- * @return {Object} :
+ *
+ * @return {Number|Object} :
+ * Если {Object}: 
  * -- response: [{id: 1},{id: 2}, {id: 3}]
- *--- свойства объекта: { id{Number}, payment{Number}, }
+ * --- свойства объекта: { id{Number}, payment{Number}, visits{Object*}}
+ * ---- visits: {today: Number, week: Number: month: Number}
  */
  
   api.get(ids)
