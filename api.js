1583a2f53bc2fce83d81b0f411ccce496568ef90
Roman Zhak <zhak55@ya.ru>
2016-06-30 14:52:33 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,9 @@
 var api = new API();
-// 1. Get a user/users 
-// @param {Number|Array|Boolean} [ids]
-// Если установлен тип Boolean, вернуть всех пользователей, которые совершали покупки.
+// 1. Получить данные по отдельному пользователю или группе.
+// @param {Number|Array|String} [ids]
+// -- {String}  :
+// --- 1. = 'count'   : Вернуть количество пользователей 
+// --- 2. = 'payment' : Вернуть пользователей, которые совершали покупки 
+// @return {Object} :
+// -- response: [{id: 1},{id: 2}, {id: 3}]
 api.get(ids)
