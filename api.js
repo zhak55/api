155e97a706569d192523910df107493c0284c465
Roman Zhak <zhak55@ya.ru>
2016-06-30 14:46:58 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,2 +1,5 @@
-// API
-// 1. Get a user / users 
+var api = new API();
+// 1. Get a user/users 
+// @param {Number|Array|Boolean} [ids]
+// Если установлен тип Boolean, вернуть всех пользователей, которые совершали покупки.
+api.get(ids)
